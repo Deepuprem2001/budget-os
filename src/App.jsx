248cc084aa0d8839b1feac7049da89dda8928599
Deepan Prashanth Prem Kumar <deepupremkumar2001@gmail.com>
2026-03-10 02:48:01 +0530

    chore: inital project setup

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+  return (
+    <div className="min-h-screen bg-gray-900 flex items-center justify-center">
+      <h1 className="text-4xl font-bold text-purple-400">
+        BudgetOS is alive 🚀
+      </h1>
+    </div>
+  )
+}
+
+export default App
